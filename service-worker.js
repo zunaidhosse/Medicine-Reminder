31df8602549c12728a25d546d1e0fd5a98fe072e
zunaidhosse <138894191+zunaidhosse@users.noreply.github.com>
2025-11-03 13:32:30 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,26 @@
-const CACHE_NAME = 'medicine-reminder-v1.2';
+const CACHE_NAME = 'medicine-reminder-v1.0';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './app.js',
-  './app-icon-192.png',
-  './app-icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.tailwindcss.com'
 ];
 
-// Install event
 self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
-  self.skipWaiting();
 });
 
-// Activate event
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating.');
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Fetch event
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        // Return cached version or fetch from network
         if (response) {
           return response;
         }
